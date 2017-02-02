d63a8f1fb6d8b0774d065f5a406d1b2db861ea1a
Neil Johnson <mr.neil.j@gmail.com>
2017-02-02 00:04:28 -0800

    added another piece                  added several other pieces of the class in                  Classes completed  Finished the project

--- main.cpp
+++ main.cpp
@@ -36,14 +36,14 @@ public:
 
 SimpleStat::SimpleStat()
 {
-    largest - sum - count = 0;
+    largest = sum = count = 0;
 }
 
 /****************************************
 *     SimpleStat::addNumber             *
 ****************************************/
 
-bool SimpleStat::addNumber(int numb)
+bool SimpleStat::addNumber(int num)
 {
     bool goodNum = true;
     if (num >= 0)               // If num is valid
@@ -99,3 +99,24 @@ double SimpleStat::calcAverage()
 /****************************************
 *               main                    *
 ****************************************/
+int main()
+{
+    int num;
+    SimpleStat statHelper;
+
+    cout << "Please enter the set of non-negative integer \n";
+    cout << "values you want to average.  Separate them with \n";
+    cout << "spaces and enter -1 after the last value.  \n\n";
+
+    cin >> num;
+    while (num >= 0)
+    {
+        statHelper.addNumber(num);
+        cin >> num;
+    }
+    cout << "\nYou entered " << statHelper.getCount() << " values. \n";
+    cout << "The largest value was " << statHelper.getLargest() << endl;
+    cout << "The average value was " << statHelper.calcAverage() << endl;
+
+    return 0;
+}
